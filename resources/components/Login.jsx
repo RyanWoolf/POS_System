1349851f190e0b0d8d8344f14356ace2f938be59
Ryan CHANG-HA LEE <13410@coderacademy.edu.au>
2023-03-23 12:54:13 +1000

    State added in Login page

--- resources/components/Login.jsx
+++ resources/components/Login.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import login_img from '../../public/images/background/login.png'
 import icon_user from '../../public/images/icons/login_user.png'
 import icon_pw from '../../public/images/icons/login_pw.png'
 
 const Login = () => {
+  const [ selectedNum, setSelectedNum ] = useState('')
+  const [ form, setForm ] = useState({
+    login_id: '',
+    password: ''
+  })
+  const [ isIDset, setIsIDset ] = useState(false)
+  const [ isPWset, setIsPWset ] = useState(false)
+  
+
+  const onInputNum = (evt) => {
+    evt.preventDefault()
+    setSelectedNum(selectedNum + evt.target.value)
+  }
+
+  const delNum = (evt) => {
+    evt.preventDefault()
+    setSelectedNum(selectedNum.slice(0, -1))
+  }
+
+  const onEnter = (evt) => {
+    evt.preventDefault()
+    const { value, name } = evt.target
+    setForm({
+      ...form,
+      [name]: value
+    })
+    // setIsIDset(true)
+  }
+
   const pin_numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
   const pin_pad = pin_numbers.map(item => {
     return <button 
             value={item}
+            onClick={onInputNum}
             className=" bg-pin_button rounded-full w-16 h-16 font-bold shadow-xl">
               {item}
           </button>
   })
+
+
 
 
 
@@ -29,34 +61,39 @@ const Login = () => {
         </div>
         <div className="flex flex-col">
           <div className="">
-            <form action="" className="m-auto">
-              <div className="flex gap-4 mb-4 pb-1 border-b-2 border-gray-400">
+
+            <form action="" className="m-auto flex gap-10">
+              { !isIDset ? (
+              // ID input
+              <div className="transition duration-300 flex justify-around gap-4 mt-8 pb-1 border-b-2 border-gray-400">
                 <img src={icon_user} alt="" />
-                <input type="text" placeholder='STAFF ID' className='bg-transparent' />
+                <input name="login_id" type="text" placeholder='STAFF ID' className='bg-transparent w-40' 
+                  value={selectedNum} />
+                <button onClick={delNum} className={!selectedNum ? 'transition duration-300 opacity-0' : 'transition duration-300 opacity-90'}>
+                  <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" fill="currentColor" clip-rule="evenodd"><path d="M7 5h17v16h-17l-7-7.972 7-8.028zm7 6.586l-2.586-2.586-1.414 1.414 2.586 2.586-2.586 2.586 1.414 1.414 2.586-2.586 2.586 2.586 1.414-1.414-2.586-2.586 2.586-2.586-1.414-1.414-2.586 2.586z"/></svg>
+                </button>
               </div> 
-              <div className="flex gap-4 pb-1 border-b-2 border-gray-400">
+              ) : (
+              // PW input
+              <div className="transition duration-300 flex justify-around gap-4 mt-8 pb-1 border-b-2 border-gray-400">
                 <img src={icon_pw} alt="" />
-                <input type="password" placeholder='PIN' className='bg-transparent' />
+                <input name="password" type="password" placeholder='PIN' className='bg-transparent w-40' 
+                  value={selectedNum} />
+                <button onClick={delNum} className={!selectedNum ? 'transition duration-300 opacity-0' : 'transition duration-300 opacity-90'}>
+                  <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" fill="currentColor" clip-rule="evenodd"><path d="M7 5h17v16h-17l-7-7.972 7-8.028zm7 6.586l-2.586-2.586-1.414 1.414 2.586 2.586-2.586 2.586 1.414 1.414 2.586-2.586 2.586 2.586 1.414-1.414-2.586-2.586 2.586-2.586-1.414-1.414-2.586 2.586z"/></svg>
+                </button>
               </div>
+              ) }
             </form>
+
+            {/* Pinpad */}
             <div className=" grid grid-cols-3 gap-7 pt-10">
               {pin_pad}
-              {/* <button className=" bg-pin_button rounded-full w-16 h-16 font-bold shadow-xl ">C</button> */}
-              <button className=" bg-pin_button rounded-full w-40 h-16 font-bold shadow-xl col-span-2 ">DEL</button>
-
-              {/* <button>7</button>
-              <button>8</button>
-              <button>9</button>
-              <button>4</button>
-              <button>5</button>
-              <button>6</button>
-              <button>1</button>
-              <button>2</button>
-              <button>3</button>
-              <button>0</button>
-              <button>DEL</button> */}
-
+              <button 
+                onClick={onEnter}
+                className=" bg-pin_button rounded-full w-40 h-16 font-bold shadow-xl col-span-2 ">ENTER</button>
             </div>
+
           </div>
         </div>
       </div>
